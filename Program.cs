708216a0d4c3a7b1291a37f36ea7fc329c9d864a
jebs01 <jebs01@gmail.com>
2019-03-14 13:36:10 +1100

    Intial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+namespace Puzzles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = AddInt(3, 4);
+            Console.WriteLine("Result: " + result);
+            Console.Read();
+        }
+
+        public static int AddInt(int x, int y)
+        {
+            return x+y;
+        }
+    }
+
+    [TestFixture]
+    class Programtests
+    {
+        [TestCase(2, 4, 6)]
+        [TestCase(1, 0, 1)]
+        [TestCase(10, -2, 8)]
+    public void TestAddition(int x,int y, int z)
+    {
+    var Results = Program.AddInt(x,y);
+    Assert.AreEqual(z,Results);
+
+    }
+}
+
+}
